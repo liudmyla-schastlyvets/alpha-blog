4c1d2cbf8b80abe68c11f23eb0358f03d34b7d81
lyudmila-zoria <mila.schastlyvets@gmail.com>
2021-01-21 22:51:51 +0200

    Added ability to delete article from browser window

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
     def show
         @article = Article.find(params[:id])
     end
@@ -33,5 +34,11 @@ class ArticlesController < ApplicationController
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 end
